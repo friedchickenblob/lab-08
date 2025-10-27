98b3729c7a35576aa32522eaaa445fa31c0a0eb0
friedchickenblob <jck2@ualberta.ca>
2025-10-26 20:04:38 -0600

    fail hascity

--- app/src/test/java/com/example/listycity/CustomListTest.java
+++ app/src/test/java/com/example/listycity/CustomListTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.ArrayList;
 
-public class CityListTest {
+public class CustomListTest {
     private CustomList mockCityList() {
         ArrayList<City> dataList = new ArrayList<>();
         dataList.add(mockCity());
@@ -24,5 +24,15 @@ public class CityListTest {
         City city = new City("Regina", "Saskatchewan");
         cityList.addCity(city);
         assertEquals(2, cityList.getCount());
+    }
+
+    @Test
+    public void testHasCity() {
+        CustomList cityList = mockCityList();
+
+        City mockCity = mockCity();
+        assertEquals(true, cityList.hasCity(mockCity));
+
+        assertEquals(false, cityList.hasCity(new City("bruh", "moment")));
     }
 }
